060b2d89a2eae57e93d284717add54bad6cf14f7
Ania <annalangner06@gmail.com>
2019-05-16 09:18:49 +0200

    Handling of content sent using the form

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,7 +1,15 @@
+var formidable = require('formidable');
+
 exports.upload = function (request, response) {
 	console.log('I start handling the upload request.');
-	response.write('I am starting upload!');
-	response.end();
+	var form = new formidable.IncomingForm();
+	form.parse(request, function (error, fields, files) {
+		fs.renameSync(files.upload.path, 'test.png');
+		response.writeHead(200, {"Content-Type": "text/html"});
+		response.write('received image:<br/>');
+		response.write('<img src="/show"/>');
+		response.end();
+	});
 }
 
 var fs = require('fs');
